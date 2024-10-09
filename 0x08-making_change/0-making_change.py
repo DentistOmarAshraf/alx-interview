5f94ff21ffe1f243e2f24d9ba455ce6539f4be76
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-09 08:22:58 +0300

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+makeChange - function count coins from total
+"""
+
+
+def makeChange(coins, total):
+    """makeChange
+    """
+    if total == 0:
+        return 0
+
+    sorted_coins = sorted(coins, reverse=True)
+    coins = 0
+    startMoney = total
+
+    for coin in sorted_coins:
+        if startMoney == 0:
+            return coins
+        if startMoney > 0 and startMoney < coin:
+            return -1
+        temp_result = startMoney // coin
+        coins += temp_result
+        startMoney -= temp_result * coin
