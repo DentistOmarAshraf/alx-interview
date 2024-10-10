2fbe5b26dd3ec1189f2d34981bfd936c79330e90
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-10 21:33:53 +0300

    ---

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,15 +12,16 @@ def island_perimeter(grid):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 start_x = x - 1 if x - 1 >= 0 else x
-                end_x = x + 1 if x + 1 <= len(grid[y]) else x
+                end_x = x + 1 if x + 1 < len(grid[x]) else x
                 for j in range(start_x, end_x + 1):
                     if grid[j][y] == 0:
                         coordinates.append((j, y))
 
                 start_y = y - 1 if y - 1 >= 0 else y
-                end_y = y + 1 if y + 1 <= len(grid[x]) else y
+                end_y = y + 1 if y + 1 < len(grid) else y
                 for j in range(start_y, end_y + 1):
                     if grid[x][j] == 0:
                         coordinates.append((x, j))
 
+    print(coordinates)
     return len(coordinates)
