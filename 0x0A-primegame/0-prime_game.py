c2683731ef72c2a77640e51f3eb76c54a258787a
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-18 04:54:39 +0300

    -----

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -21,6 +21,8 @@ def is_prime(num):
 def isWinner(x, nums):
     """isWinner
     """
+    if len(nums) != x:
+        return None
     Maria_score, Ben_score = 0, 0
     for i in range(x):
         Maria_turn, Ben_turn = True, False
